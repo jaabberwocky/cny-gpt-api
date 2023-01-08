ae1dcd297b0995af67d716d3cdd6842251781e5a
tobias <tobias.leong@zendesk.com>
2023-01-08 11:53:53 +0800

    Convert ping to non-plugin

--- src/routes/ping.ts
+++ src/routes/ping.ts
@@ -1,16 +1,16 @@
 import {
     FastifyInstance,
     FastifyReply,
-    FastifyRequest,
-    FastifyPluginAsync,
+    FastifyRequest
 } from 'fastify'
 
 const pong = (request: FastifyRequest, reply: FastifyReply) => {
     reply.status(200).send({ msg: 'pong' })
 }
 
-const ping: FastifyPluginAsync = async (fastify: FastifyInstance, opts) => {
+const ping =  (fastify: FastifyInstance, opts: any, done: any) => {
     fastify.get('/ping', pong)
+    done()
 }
 
 export default ping
